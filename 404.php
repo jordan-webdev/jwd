02685b73a95e342ba28dd79f4bd78c5fad8b7186
jordan-webdev <jordan.trombone@gmail.com>
2017-09-05 14:57:46 -0400

    Update 404.php

--- 404.php
+++ 404.php
@@ -9,15 +9,15 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area padding-site">
+	<main id="main" class="site-main container-site">
 
 		<section id="post-<?php the_ID(); ?>" <?php post_class('error-404'); ?>>
-			<header class="entry-header">
+			<header class="entry-header mar-t-25">
 				<h1 class="entry-title">This page does not exist.</h1>
 			</header><!-- .entry-header -->
 
-			<div class="entry-content">
+			<div class="entry-content mar-b-35">
 				<a class="visit-home-page color-primary" href="<?php echo get_home_url(); ?>">Click here to visit the home page.</a>
 			</div><!-- .entry-content -->
 		</section><!-- #post-## -->
@@ -27,4 +27,3 @@ get_header(); ?>
 
 <?php
 get_footer();
-
