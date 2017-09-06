865ba620e14086a2bfa4e8f2e3bda8cdbfc3ce4e
jordan-webdev <jordan.trombone@gmail.com>
2017-09-06 10:07:54 -0400

    Update theme-functions.php

--- inc/functions/theme-functions.php
+++ inc/functions/theme-functions.php
@@ -1,11 +1,3 @@
 <?php
 $template = get_template_directory();
 
-require $template . '/inc/functions/html-helper-functions.php';
-require $template . '/inc/functions/ACF-functions.php';
-
-//Components
-require $template . '/inc/functions/components/components.php';
-
-//Shortcodes
-require $template . '/inc/functions/shortcodes.php';
