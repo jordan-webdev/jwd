47b9e355b5efd3919442c33400a765226322bd75
jordan-webdev <jordan.trombone@gmail.com>
2017-09-11 14:14:23 -0400

    Update ACF-functions.php

--- inc/functions/ACF-functions.php
+++ inc/functions/ACF-functions.php
@@ -20,6 +20,12 @@ if (function_exists('acf_add_options_page')) {
         'menu_title'    => 'Footer',
         'parent_slug'    => 'theme-general-settings',
     ));
+
+    acf_add_options_sub_page(array(
+        'page_title'    => 'Theme Schema Settings',
+        'menu_title'    => 'Schema',
+        'parent_slug'    => 'theme-general-settings',
+    ));
 }
 
 //Populate social media choices with values from options repeater field
