a094c434284589fe0c4f33a6146f4e025fe796ec
jordan-webdev <jordan.trombone@gmail.com>
2017-09-12 13:20:10 -0400

    Add files via upload

--- inc/functions/custom_post_types.php
+++ inc/functions/custom_post_types.php
@@ -1,1 +1,89 @@
 <?php
+
+// FAQ Custom Post Type
+ function register_faq() {
+
+ $labels = array(
+ 'name' => __( 'FAQ'),
+ 'singular_name' => __( 'FAQ'),
+ 'all_items' => __( 'All FAQ'),
+ 'add_new' => __( 'Add New FAQ'),
+ 'add_new_item' => __( 'Add New FAQ'),
+ 'edit_item' => __( 'Edit FAQ'),
+ 'new_item' => __( 'New FAQ'),
+ 'view_item' => __( 'View FAQ'),
+ 'not_found' => __( 'No FAQ '),
+ 'not_found_in_trash' => __( 'No FAQ found in Trash'),
+ );
+
+ $args = array(
+ 'label'               => __( 'FAQ' ),
+ 'description'         => __( ''),
+ 'labels'              => $labels,
+ 'supports'            => array( 'title', 'editor', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+ 'hierarchical'        => false,
+ 'public'              => true,
+ 'show_ui'             => true,
+ 'show_in_menu'        => true,
+ 'show_in_nav_menus'   => true,
+ 'show_in_admin_bar'   => true,
+ 'can_export'          => true,
+ 'has_archive'         => true,
+ 'exclude_from_search' => false,
+ 'publicly_queryable'  => true,
+ 'capability_type'     => 'page',
+ 'menu_icon'           => 'dashicons-editor-help',
+ // This is where we add taxonomies to our CPT
+ 'taxonomies'          => array( 'faq_category' ),
+ );
+
+ register_post_type( 'faq', $args );
+
+ }
+
+ add_action( 'init', 'register_faq', 0 );
+
+ // Blog Custom Post Type
+function register_blog_posts()
+{
+    $labels = array(
+    'name' => __('Blog Posts'),
+    'singular_name' => __('Blog Post'),
+    'all_items' => __('All Blog Posts'),
+    'add_new' => __('Add new Blog Post'),
+    'add_new_item' => __('Add new Blog Post'),
+    'edit_item' => __('Edit Blog Post'),
+    'new_item' => __('New Blog Post'),
+    'view_item' => __('View Blog Post'),
+    'not_found' => __('No Blog Posts found'),
+    'not_found_in_trash' => __('No Blog Posts found in Trash'),
+);
+
+    $supports = array(
+    'title',
+    'editor',
+    'author',
+    'thumbnail',
+    'comments',
+    'custom-fields',
+    'revisions',
+    'page-attributes',
+    'post-formats',
+);
+
+    $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'publicly_queriable' => true,  // you should be able to query it
+    'capability_type'     => 'page',
+    'show_ui' => true,  // you should be able to edit it in wp-admin
+    'exclude_from_search' => false,  // you should exclude it from search results
+    'show_in_nav_menus' => true,  // you shouldn't be able to add it to menus
+    'has_archive' => false,  // it shouldn't have archive page
+    'supports' => $supports,
+    'menu_icon' => 'dashicons-welcome-write-blog',
+    //'taxonomies' => array( 'category' ),
+);
+    register_post_type('blog_posts', $args);
+}
+add_action('init', 'register_blog_posts', 0);
--- inc/functions/taxonomies.php
+++ inc/functions/taxonomies.php
@@ -1,1 +1,64 @@
 <?php
+
+// Add the "faq_category" taxonomy
+function create_faq_category_hierarchical_taxonomy() {
+
+  $labels = array(
+    'name' => _x( 'FAQ Category', 'taxonomy general name' ),
+    'singular_name' => _x( 'FAQ Category', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search FAQ Categories' ),
+    'all_items' => __( 'All FAQ Categories' ),
+    'parent_item' => __( 'Parent FAQ Category' ),
+    'parent_item_colon' => __( 'Parent FAQ Category:' ),
+    'edit_item' => __( 'Edit FAQ Category' ),
+    'update_item' => __( 'Update FAQ Category' ),
+    'add_new_item' => __( 'Add New FAQ Category' ),
+    'new_item_name' => __( 'New FAQ Category Name' ),
+    'menu_name' => __( 'FAQ Categories' ),
+  );
+
+  register_taxonomy(
+    'faq_category',
+    'faq',
+    array(
+      'hierarchical' => true,
+      'labels' => $labels,
+      'show_ui' => true,
+      'show_admin_column' => true,
+      'query_var' => true,
+    ));
+
+}
+add_action( 'init', 'create_faq_category_hierarchical_taxonomy', 0 );
+
+// Add the "Blog Category" taxonomy
+function create_blog_hierarchical_taxonomy() {
+
+  $labels = array(
+    'name' => _x( 'Blog Categories', 'taxonomy general name' ),
+    'singular_name' => _x( 'Blog Category', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Blog Categories' ),
+    'all_items' => __( 'All Blog Categories' ),
+    'parent_item' => __( 'Parent Blog Category' ),
+    'parent_item_colon' => __( 'Parent Blog Category:' ),
+    'edit_item' => __( 'Edit Blog Category' ),
+    'update_item' => __( 'Update Blog Category' ),
+    'add_new_item' => __( 'Add New Blog Category' ),
+    'new_item_name' => __( 'New Blog category Name' ),
+    'menu_name' => __( 'Blog Categories' ),
+  );
+
+  register_taxonomy(
+    'blog_category',
+    'blog_posts',
+    array(
+      'hierarchical' => true,
+      'labels' => $labels,
+      'show_ui' => true,
+      'show_admin_column' => true,
+      'query_var' => true,
+      'rewrite' => array( 'slug' => 'blog' ),
+    ));
+
+}
+add_action( 'init', 'create_blog_hierarchical_taxonomy', 0 );
