4444b6e47d82237b9632b826c3f83d5230593313
jordan-webdev <jordan.trombone@gmail.com>
2017-09-12 13:11:08 -0400

    Add files via upload

---
+++ template-parts/blog/part-category-description-column.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Template part for displaying the details of the categories chosen from the select category menu
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package jwd
+ */
+
+$blog_category = get_the_terms($post->ID, 'blog_category')[0];
+$blog_category_slug = $blog_category->slug;
+$blog_category_name = $blog_category->name;
+$single_slug = $post->post_name;
+?>
+
+		<?php
+			$external = get_field('external_link');
+			$youtube = get_field('youtube_link');
+			$pdf = get_field('pdf_upload');
+			$preview = get_field('preview_image');
+
+			$link_types = array(
+				$external => "Go to Article",
+				$youtube => "Watch Video",
+				$pdf => "Download PDF",
+			);
+			$link = "";
+			$link_text = "";
+			foreach($link_types as $url => $text ){
+				if ($url){
+					$link = $url;
+					$link_text = $text;
+				}
+			}
+			$external_link = $link ? true : false;
+			$link_text = $link_text ? $link_text : "Read More";
+			$link = $link ? $link : get_the_permalink();
+		?>
+
+    <div class="pad-b-15 pad-l-15 pad-r-15 blog-item-padding-wrapper">
+  		<article class="blog-item-wrapper clear <?php echo $blog_category_slug; ?> all">
+  			<div class="blog-preview-item">
+  				<?php if ($external_link) : ?>
+  					<a href="<?php echo esc_url($link); ?>" target="_blank" rel="noopener noreferrer">
+  						<?php
+  							if ($preview) {
+  								echo '<img class="news-thumb-preview block" src="' . $preview['url'] . '" alt="' . $preview['alt'] . '" />';
+  							}
+  						?>
+  					</a>
+  				<?php else: ?>
+            <!-- Preview image for internal stories -->
+  					<a
+              class="js-blog-ajax-link"
+              href="<?php echo esc_url($link); ?>"
+              data-slug="<?php echo $blog_category_slug; ?>"
+              data-single-slug="<?php echo $single_slug; ?>"
+            >
+  						<?php
+  							$preview = get_field('preview_image');
+  							if ($preview)
+  								echo '<img class="news-thumb-preview block" src="' . $preview['url'] . '" alt="' . $preview['alt'] . '" />';
+  						?>
+  					</a>
+  				<?php endif; ?>
+  			</div>
+  			<div class="blog-preview-item">
+  				<?php if ($external_link): ?>
+            <!-- External Article/PDF/Youtube -->
+						<!-- Category -->
+						<span class="blog-preview__cat color-primary font-14"><?php echo esc_html($blog_category_name); ?></span>
+  					<a href="<?php echo esc_url($link); ?>" target="_blank" rel="noopener noreferrer">
+  						<h3
+                class="color-secondary news-preview-title font-18 mar-t-10 mar-b-10"
+                data-slug="<?php echo $blog_category_slug; ?>"
+                data-single-slug="<?php echo $single_slug; ?>"
+              >
+                <?php echo get_the_title(); ?>
+              </h3>
+  					</a>
+						<!-- Date -->
+						<div class="news-preview-date-wrapper">
+	            <!--<span class="fa fa-calendar-o color-primary" aria-hidden="true"></span>-->
+	  					<span class="blog-preview-author color-dark-grey font-14 block mar-b-10"><?php echo get_the_date('F j, Y'); ?></span>
+	  				</div>
+  					<p class="body-font news-media-excerpt font-14 color-black"><?php echo get_the_content(); ?></p>
+  					<a class="btn btn--has-arrow" href="<?php echo esc_url($link); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html($link_text); ?></a>
+  				<?php else: ?>
+            <!-- Internal Story -->
+						<!-- Category -->
+						<span class="blog-preview__cat color-primary font-14"><?php echo esc_html($blog_category_name); ?></span>
+  					<a
+              href="<?php echo esc_url($link); ?>"
+              class="js-blog-ajax-link"
+              data-slug="<?php echo $blog_category_slug; ?>"
+              data-single-slug="<?php echo $single_slug; ?>"
+            >
+              <h3 class="color-secondary news-preview-title font-18 mar-t-10 mar-b-10">
+                <?php echo get_the_title(); ?>
+              </h3>
+  					</a>
+						<!-- Date -->
+						<div class="news-preview-date-wrapper">
+	            <!--<span class="fa fa-calendar-o color-primary" aria-hidden="true"></span>-->
+	  					<span class="blog-preview-author color-dark-grey font-14 block mar-b-10"><?php echo get_the_date('F j, Y'); ?></span>
+	  				</div>
+  					<p class="body-font news-media-excerpt font-14 color-black"><?php echo get_excerpt(150, false); ?></p>
+						<a class="btn btn--has-arrow js-ajax-link" href="<?php echo esc_url($link); ?>"><?php echo esc_html($link_text); ?></a>
+  				<?php endif; ?>
+
+  			</div>
+  		</article>
+    </div>
---
+++ template-parts/blog/part-category-select-column.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Template part for displaying a select list of the categories in a column.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ */
+
+// This module requires a custom hierarchical taxonomy to be created, with "All" as the ancestor
+
+
+$query_obj = get_queried_object();
+
+$term_name = $query_obj->name;
+$taxonomy = $query_obj->taxonomy;
+// If it's a single blog post, the query object will not contain data about the taxonomy, so it's specified here
+$taxonomy = $taxonomy ? $taxonomy : 'blog_category';
+$all_term = get_term_by( 'slug', 'all', $taxonomy );
+$all_term_id = $all_term->term_id;
+
+?>
+
+<div id="category-wrapper">
+
+	<h2 id="js-category-wrapper-title" class="font-16 bold color-secondary js-categories-wrapper-toggle">
+    CATEGORIES
+    <span class="screen-reader-text">Click to expand categories</span>
+  </h2>
+
+	<div id="categories-wrapper">
+
+		<div id="categories">
+
+      <?php
+        $args = array(
+    			'child_of' => $all_term_id,
+    			'taxonomy' => $taxonomy,
+    		);
+
+			$categories = get_categories($args); ?>
+
+			<!-- All -->
+				<a href="<?php echo get_term_link($all_term_id); ?>" data-slug="all" class="js-category-selector-link">
+					<span
+            class="block all-categories-selector sub-header <?php echo ($term_name == "All" ? "color-primary js-selected-category" : ""); ?>"
+            data-slug="all"
+          >
+            All
+            <i class="fa fa-chevron-right" aria-hidden="true"></i>
+        </span>
+				</a>
+
+
+			<!-- Categories -->
+			<?php foreach ($categories as $category) :
+        //var_dump($category);
+        $id = $category->term_id;
+        $data_category = $category->category_nicename;
+        $label = $category->name;
+        $slug = $category->slug;
+
+        $is_listing_match = $term_name == $label ? true : false;
+      ?>
+  			<a href="<?php echo get_term_link($id); ?>" data-slug="<?php echo $slug; ?>" class="js-category-selector-link">
+  				<span
+            class="block sub-header <?php echo ($is_listing_match ? "color-primary js-selected-category" : ""); ?>"
+            data-slug="<?php echo $data_category; ?>"
+          >
+            <?php echo $label; ?>
+            <i class="fa fa-chevron-right black" aria-hidden="true"></i>
+          </span>
+  			</a>
+
+			<?php endforeach; ?>
+
+		</div>
+
+	</div>
+
+</div>
