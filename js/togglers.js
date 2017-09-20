838a1187ac4ca7b40e0d7a1c93dff8ded08f98e9
jordan-webdev <jordan.trombone@gmail.com>
2017-09-20 15:26:22 -0400

    Update togglers.js

--- js/togglers.js
+++ js/togglers.js
@@ -9,14 +9,6 @@
     });
   })
 
-  // Our services return toggle
-  $('.our-services__list').on('click', '.our-services__list-item.is-shown', function() {
-    var toggler = $('.our-services__ddtoggle');
-    $('.our-services__list-item').toggleClass('is-shown');
-    $('#js-our-services__drop-list').slideToggle();
-  });
-
-
   function slide_toggler(toggler, toggle_item, return_toggler) {
     toggler.toggleClass('is-active');
     $('.' + return_toggler).toggleClass('is-shown');
