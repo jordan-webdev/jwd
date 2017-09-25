e5312a947e7d4116a2c6cbcd2f139cb1a83fae33
jordan-webdev <jordan.trombone@gmail.com>
2017-09-25 10:57:49 -0400

    Update part-header-mobile.php

--- template-parts/header/part-header-mobile.php
+++ template-parts/header/part-header-mobile.php
@@ -6,14 +6,16 @@
 
 <div class="header-mobile color-primary flex justify-between align-center width-100">
 
+  <!-- Menu -->
   <button id="js-mobile-menu-toggle" class="header-mobile__button header-mobile__icon color-white" aria-controls="primary-menu" aria-expanded="false" data-toggles="#mobile-menu">
     <!--<img src="<?php //echo $images; ?>/hamburger-min.png" alt="Toggle menu." />-->
     <span class="fa fa-bars header-mobile__hamburger" aria-hidden="true"></span>
     <span class="screen-reader-text">Click to toggle the navigation menu.</span>
   </button>
 
+  <!-- Logo -->
   <a class="header-mobile__logo" href="<?php echo get_home_url(); ?>" rel="home">
-    <img src="<?php echo get_header_image(); ?>" alt="<?php bloginfo("title"); ?>">
+    <img class="width-100" src="<?php echo get_field("mobile_logo", "options"); ?>" alt="<?php bloginfo("title"); ?>">
     <span class="screen-reader-text"><?php bloginfo("name"); ?>
     </span>
   </a>
