ef27d08e16224194c1e389799cc23c3bb61ee80f
jordan-webdev <jordan.trombone@gmail.com>
2017-10-17 16:51:31 -0400

    Update template-shortcodes.php

--- inc/functions/template-shortcodes/template-shortcodes.php
+++ inc/functions/template-shortcodes/template-shortcodes.php
@@ -1,3 +1,4 @@
 <?php
 
 require get_template_directory() .'/inc/functions/template-shortcodes/display_careers.php';
+require get_template_directory() .'/inc/functions/template-shortcodes/side_items.php';
