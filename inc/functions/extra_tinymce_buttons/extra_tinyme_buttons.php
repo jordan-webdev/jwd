c8b0a3cfc51361f97a894077ba521156f5473547
jordan-webdev <jordan.trombone@gmail.com>
2017-10-18 10:45:57 -0400

    Update extra_tinyme_buttons.php

--- inc/functions/extra_tinymce_buttons/extra_tinyme_buttons.php
+++ inc/functions/extra_tinymce_buttons/extra_tinyme_buttons.php
@@ -1,13 +1,13 @@
 <?php
 
-function enqueue_plugin_scripts($plugin_array)
+function enqueue_extra_tinymce_buttons_scripts($plugin_array)
 {
     //enqueue TinyMCE plugin script with its ID.
     $plugin_array["shortcode_buttons"] =  get_template_directory_uri() . "/inc/functions/extra_tinymce_buttons/index.js";
     return $plugin_array;
 }
 
-add_filter("mce_external_plugins", "enqueue_plugin_scripts");
+add_filter("mce_external_plugins", "enqueue_extra_tinymce_buttons_scripts");
 
 function add_tinymce_buttons($buttons)
 {
