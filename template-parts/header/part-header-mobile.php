546959b5e2b19f5a4c0a31260813974b9f7b79b3
jordan-webdev <jordan.trombone@gmail.com>
2017-10-26 16:10:44 -0400

    Update part-header-mobile.php

--- template-parts/header/part-header-mobile.php
+++ template-parts/header/part-header-mobile.php
@@ -4,26 +4,31 @@
 */
 ?>
 
-<div class="header-mobile color-primary flex justify-between align-center width-100">
+<div class="padding-site">
+  <div class="container-site">
+    <nav class="header-mobile color-primary flex justify-between align-center width-100">
 
-  <!-- Menu -->
-  <button id="js-mobile-menu-toggle" class="header-mobile__button header-mobile__icon color-white" aria-controls="mobile-menu" aria-expanded="false" data-toggles="#mobile-menu">
-    <!--<img src="<?php //echo $images; ?>/hamburger-min.png" alt="Toggle menu." />-->
-    <span class="fa fa-bars header-mobile__hamburger" aria-hidden="true"></span>
-    <span class="screen-reader-text">Click to toggle the navigation menu.</span>
-  </button>
+      <!-- Menu -->
+      <button id="js-mobile-menu-toggle" class="header-mobile__button header-mobile__icon color-primary" aria-controls="mobile-menu" aria-expanded="false" data-toggles="#mobile-menu">
+        <!--<img src="<?php //echo $images; ?>/hamburger-min.png" alt="Toggle menu." />-->
+        <span class="fa fa-bars header-mobile__hamburger" aria-hidden="true"></span>
+        <span class="screen-reader-text">Click to toggle the navigation menu.</span>
+      </button>
 
-  <!-- Logo -->
-  <a class="header-mobile__logo" href="<?php echo get_home_url(); ?>" rel="home">
-    <img class="width-100" src="<?php echo get_field("mobile_logo", "options"); ?>" alt="<?php bloginfo("title"); ?>">
-    <span class="screen-reader-text"><?php bloginfo("name"); ?>
-    </span>
-  </a>
+      <!-- Logo -->
+      <a class="header-mobile__logo" href="<?php echo get_home_url(); ?>" rel="home">
+        <img class="width-100" src="<?php echo get_field("mobile_logo", "options"); ?>" alt="<?php bloginfo("title"); ?>">
+        <span class="screen-reader-text"><?php bloginfo("name"); ?>
+        </span>
+      </a>
 
-  <!-- Phone -->
-  <a class="header-mobile__phone header-mobile__icon color-white" href="tel:<?php echo esc_attr(get_field("site_phone", "options")); ?>"><span class="fa fa-phone" aria-hidden="true"></span></a>
+      <!-- Phone -->
+      <a class="header-mobile__phone header-mobile__icon color-white" href="tel:<?php echo esc_attr(get_field("site_phone", "options")); ?>"><span class="fa fa-phone" aria-hidden="true"></span></a>
 
+    </nav>
+  </div>
 </div>
+
 
 <!-- Mobile navigation (placed outside header-mobile to prevent flex miscalculation) -->
 <nav id="mobile-site-navigation" class="mobile-navigation header-mobile__navigation">
