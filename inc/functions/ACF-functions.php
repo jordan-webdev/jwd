18eb942c1564f737c2241e5a10494d3e0631a8dc
jordan-webdev <jordan.trombone@gmail.com>
2017-11-15 09:38:09 -0500

    Update ACF-functions.php

--- inc/functions/ACF-functions.php
+++ inc/functions/ACF-functions.php
@@ -149,3 +149,54 @@ function acf_location_rule_match_category_ancestor($match, $rule, $options)
 
 // Show WP custom field metabox
 add_filter('acf/settings/remove_wp_meta_box', '__return_false');
+
+
+/* *****************************************************
+ * ********** Custom fields for specific category
+ * *****************************************************
+ */
+
+
+add_filter('acf/location/rule_types', 'acf_location_rules_types', 999);
+function acf_location_rules_types($choices) {
+    // create a new group for the rules called Terms
+    // if it does not already exist
+    if (!isset($choices['Terms'])) {
+        $choices['Terms'] = array();
+    }
+    // create new rule type in the new group
+    $choices['Terms']['category_id'] = 'Category Name';
+    return $choices;
+}
+
+add_filter('acf/location/rule_values/category_id', 'acf_location_rules_values_category');
+function acf_location_rules_values_category($choices) {
+    // get terms and build choices
+    $taxonomy = 'category';
+    $args = array('hide_empty' => false);
+    $terms = get_terms($taxonomy, $args);
+    if (count($terms)) {
+        foreach ($terms as $term) {
+            $choices[$term->term_id] = $term->name;
+        }
+    }
+    return $choices;
+}
+
+add_filter('acf/location/rule_match/category_id', 'acf_location_rules_match_category', 10, 3);
+function acf_location_rules_match_category($match, $rule, $options) {
+    if (!isset($_GET['tag_ID']) ||
+            !isset($_GET['taxonomy']) ||
+            $_GET['taxonomy'] != 'category') {
+        // bail early
+        return $match;
+    }
+    $term_id = $_GET['tag_ID'];
+    $selected_term = $rule['value'];
+    if ($rule['operator'] == '==') {
+        $match = ($selected_term == $term_id);
+    } elseif ($rule['operator'] == '!=') {
+        $match = ($selected_term != $term_id);
+    }
+    return $match;
+}
