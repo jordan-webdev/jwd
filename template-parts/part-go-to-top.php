f5aedf83fa164e0465f8163717d5a9f0848d2232
jordan-webdev <jordan.trombone@gmail.com>
2018-02-01 17:51:20 -0500

    Create part-go-to-top.php

---
+++ template-parts/part-go-to-top.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Go to top button
+ */
+
+$images = get_template_directory_uri() . '/images';
+?>
+
+<a class="go-to-top" href="#masthead">
+  <img src="<?php echo $images; ?>/faq-arrow-min.png" alt="Click to go to top">
+</a>
