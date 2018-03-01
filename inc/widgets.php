6762c0820b0cf606f13e3da7790c61fb749d2109
jordan-webdev <jordan.trombone@gmail.com>
2018-03-01 17:32:34 -0500

    Update widgets.php

--- inc/widgets.php
+++ inc/widgets.php
@@ -6,15 +6,17 @@
  */
 function jwd_widgets_init()
 {
-    register_sidebar(array(
-        'name'          => esc_html__('Sidebar', 'jwd'),
-        'id'            => 'sidebar-1',
-        'description'   => esc_html__('Add widgets here.', 'jwd'),
-        'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h2 class="widget-title">',
-        'after_title'   => '</h2>',
-    ));
+    for ($i = 1; $i <= 9; $i++) {
+        register_sidebar(array(
+          'name'          => esc_html__('Widget ' .$i, 'jwd'),
+          'id'            => 'widget-' .$i,
+          'description'   => esc_html__('Add widgets here.', 'jwd'),
+          'before_widget' => '',
+          'after_widget'  => '',
+          'before_title'  => '<h2 class="sitemap__item-title">',
+          'after_title'   => '</h2>',
+      ));
+    }
 
     for ($i = 1; $i <= 9; $i++) {
         register_sidebar(array(
@@ -23,7 +25,7 @@ function jwd_widgets_init()
           'description'   => esc_html__('Add widgets here.', 'jwd'),
           'before_widget' => '',
           'after_widget'  => '',
-          'before_title'  => '<h2 class="sitemap__item-title color-primary">',
+          'before_title'  => '<h2 class="sitemap__item-title">',
           'after_title'   => '</h2>',
       ));
     }
