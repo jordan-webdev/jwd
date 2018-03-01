d627c882eea3fbbfef7906de246ec8a758f156ee
jordan-webdev <jordan.trombone@gmail.com>
2018-03-01 17:29:56 -0500

    Update custom_post_types.php

--- inc/functions/custom_post_types.php
+++ inc/functions/custom_post_types.php
@@ -69,7 +69,7 @@
  'show_in_nav_menus'   => true,
  'show_in_admin_bar'   => true,
  'can_export'          => true,
- 'has_archive'         => true,
+ 'has_archive'         => false,
  'exclude_from_search' => false,
  'publicly_queryable'  => true,
  'capability_type'     => 'page',
@@ -83,6 +83,49 @@
  }
 
  add_action( 'init', 'register_faq', 0 );
+
+ // Glossary Custom Post Type
+  function register_glossary() {
+
+  $labels = array(
+  'name' => __( 'Glossary'),
+  'singular_name' => __( 'Glossary'),
+  'all_items' => __( 'All Glossary'),
+  'add_new' => __( 'Add New Glossary'),
+  'add_new_item' => __( 'Add New Glossary'),
+  'edit_item' => __( 'Edit Glossary'),
+  'new_item' => __( 'New Glossary'),
+  'view_item' => __( 'View Glossary'),
+  'not_found' => __( 'No Glossary found'),
+  'not_found_in_trash' => __( 'No Glossary found in Trash'),
+  );
+
+  $args = array(
+  'label'               => __( 'Glossary' ),
+  'description'         => __( ''),
+  'labels'              => $labels,
+  'supports'            => array( 'title', 'editor', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+  'hierarchical'        => false,
+  'public'              => true,
+  'show_ui'             => true,
+  'show_in_menu'        => true,
+  'show_in_nav_menus'   => true,
+  'show_in_admin_bar'   => true,
+  'can_export'          => true,
+  'has_archive'         => false,
+  'exclude_from_search' => false,
+  'publicly_queryable'  => true,
+  'capability_type'     => 'page',
+  'menu_icon'           => 'dashicons-editor-spellcheck',
+  // This is where we add taxonomies to our CPT
+  'taxonomies'          => array( 'glossary_category' ),
+  );
+
+  register_post_type( 'glossary', $args );
+
+  }
+
+  add_action( 'init', 'register_glossary', 0 );
 
  // Blog Custom Post Type
 function register_blog_posts()
