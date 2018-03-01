f73384335b78a62ad42f9f4ce8f0c6859a641c4d
jordan-webdev <jordan.trombone@gmail.com>
2018-03-01 17:30:54 -0500

    Update taxonomies.php

--- inc/functions/taxonomies.php
+++ inc/functions/taxonomies.php
@@ -31,6 +31,37 @@ function create_faq_category_hierarchical_taxonomy() {
 }
 add_action( 'init', 'create_faq_category_hierarchical_taxonomy', 0 );
 
+// Add the "Glossary Category" taxonomy
+function create_glossary_category_taxonomy() {
+
+  $labels = array(
+    'name' => _x( 'Glossary Category', 'taxonomy general name' ),
+    'singular_name' => _x( 'Glossary Category', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Glossary Categories' ),
+    'all_items' => __( 'All Glossary Categories' ),
+    'parent_item' => __( 'Parent Glossary Category' ),
+    'parent_item_colon' => __( 'Parent Glossary Category:' ),
+    'edit_item' => __( 'Edit Glossary Category' ),
+    'update_item' => __( 'Update Glossary Category' ),
+    'add_new_item' => __( 'Add New Glossary Category' ),
+    'new_item_name' => __( 'New Glossary Category Name' ),
+    'menu_name' => __( 'Glossary Categories' ),
+  );
+
+  register_taxonomy(
+    'glossary_category',
+    'glossary',
+    array(
+      'hierarchical' => true,
+      'labels' => $labels,
+      'show_ui' => true,
+      'show_admin_column' => true,
+      'query_var' => true,
+    ));
+
+}
+add_action( 'init', 'create_glossary_category_taxonomy', 0 );
+
 // Add the "Blog Category" taxonomy
 function create_blog_hierarchical_taxonomy() {
 
