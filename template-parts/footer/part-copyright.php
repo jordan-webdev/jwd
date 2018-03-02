feb93c9c1b17c248b0c2bbf8001edf55e703f63d
jordan-webdev <jordan.trombone@gmail.com>
2018-03-01 21:05:22 -0500

    Update part-copyright.php

--- template-parts/footer/part-copyright.php
+++ template-parts/footer/part-copyright.php
@@ -3,16 +3,14 @@
  * Copyright
  * Copyright text with year, Dolce Media Link
  */
+
+$d = get_field("disclaimer", "options");
 ?>
 
-<div class="copyright__wrapper padding-site color-tertiary--bg">
-  <p class="copyright container-site">
-    <span class="copyright__site">
+<div class="disclaimer">
+  <p class="text"><?php echo esc_html($d); ?></p>
+  <p class="copyright">
       Copyright &copy; <?php echo Date('Y'); ?>. All Rights Reserved <?php bloginfo('title'); ?>
-    </span>
-    <span class="copyright__dolce">
-      Designed and developed by
-      <a href="http://www.dolcemedia.ca/" rel="noopener noreferrer" target="_blank">Dolce Media Group</a>
-    </span>
+      Designed and developed by <a href="http://www.dolcemedia.ca/" rel="noopener noreferrer" target="_blank">Dolce Media Group</a>
   </p>
 </div>
