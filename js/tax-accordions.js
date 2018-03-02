d310c9491e01cebcb0794bbc41afda765f1223e9
jordan-webdev <jordan.trombone@gmail.com>
2018-03-01 23:28:55 -0500

    Update tax-accordions.js

--- js/tax-accordions.js
+++ js/tax-accordions.js
@@ -1,11 +1,11 @@
 (function($) {
 
-  $('.tax-accordions__pullout').slideUp(1);
-  $('body').on('click', '.tax-accordions__toggler', function() {
+  $('.accordions__pullout').slideUp(1);
+  $('body').on('click', '.accordions__toggler', function() {
     var arrow = $(this);
-    var pullout = $(this).next('.tax-accordions__pullout');
-    $(this).parent('.tax-accordions__accordion').toggleClass('active');
-    $('.tax-accordions__pullout').not(pullout).slideUp().toggleClass('active');
+    var pullout = $(this).next('.accordions__pullout');
+    $(this).parent('.accordions__accordion').toggleClass('active');
+    $('.accordions__pullout').not(pullout).slideUp().toggleClass('active');
     pullout.slideToggle();
   });
 
