2f97ac1888f496350e3d3d68bfa36581a61b4c59
jordan-webdev <jordan.trombone@gmail.com>
2018-03-02 17:36:09 -0500

    Update part-middle-bar.php

--- template-parts/header/part-middle-bar.php
+++ template-parts/header/part-middle-bar.php
@@ -6,27 +6,8 @@
 
 <div class="grid-x align-center">
 
-  <!-- Since Img -->
-  <div class="cell large-4 since-img">
-    <?php if (is_active_sidebar("widget-1")): ?>
-      <?php dynamic_sidebar("widget-1") ?>
-    <?php endif; ?>
-    <?php if (is_active_sidebar("widget-2")): ?>
-      <?php dynamic_sidebar("widget-2") ?>
-    <?php endif; ?>
-
-    <!-- Button -->
-    <?php if (is_active_sidebar("widget-5")): ?>
-      <?php dynamic_sidebar("widget-5") ?>
-    <?php endif; ?>
-    <?php if (is_active_sidebar("widget-6")): ?>
-      <?php dynamic_sidebar("widget-6") ?>
-    <?php endif; ?>
-  </div>
-
-
   <!-- Logo -->
-  <div class="desktop-logo middle-bar__logo cell large-4 justify-center">
+  <div class="logo">
     <?php if (is_front_page()): ?>
       <h1>
     <?php endif; ?>
@@ -38,15 +19,5 @@
       </h1>
     <?php endif; ?>
   </div>
-
-  <!-- Buttons -->
-  <nav class="cell medium-4 btns flex justify-end buttons">
-    <?php if (is_active_sidebar("widget-3")): ?>
-      <?php dynamic_sidebar("widget-3") ?>
-    <?php endif; ?>
-    <?php if (is_active_sidebar("widget-4")): ?>
-      <?php dynamic_sidebar("widget-4") ?>
-    <?php endif; ?>
-  </nav>
 
 </div>
