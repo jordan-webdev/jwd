d8be32163191da1100948541c7889a1241674e39
jordan-webdev <jordan.trombone@gmail.com>
2018-03-01 21:08:17 -0500

    Update part-site-map.php

--- template-parts/footer/part-site-map.php
+++ template-parts/footer/part-site-map.php
@@ -43,39 +43,7 @@ $all_items = array();
         <li class="sitemap__item"><?php echo preg_replace($search, $replace, $item); ?></li>
       <?php endforeach; ?>
     </ul>
-
-    <div class="need-help">
-      <?php
-        $nh = get_field("need_help", "options");
-        $title = $nh['title'];
-        $phone = $nh['phone'];
-        $email = $nh['email'];
-        $social = $nh['social'];
-      ?>
-      <h2 class="title"><?php echo esc_html($title); ?></h2>
-      <div class="text">
-        <a class="link block" href="tel:<?php echo esc_attr(str_replace(" ", "", $phone)); ?>"><?php echo esc_html($phone); ?></a>
-        <a class="link block" href="mailto:<?php echo esc_attr($email); ?>"><?php echo esc_html($email); ?></a>
-      </div>
-      <ul class="footer-social list">
-        <?php foreach ($social as $s):
-          $link = $s['link'];
-          $img = $s['img'];
-          $label = $s['label'];
-        ?>
-          <li class="social-item item">
-            <a class="social-link" href="<?php echo esc_url($link); ?>">
-              <?php echo wp_get_attachment_image( $img, "full", false, array("class" => "icon") ); ?>
-              <span class="screen-reader-text"><?php echo esc_html($label); ?></span>
-            </a>
-          </li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
   </div>
-
-
-
 </section>
 
 <!-- Mobile -->
