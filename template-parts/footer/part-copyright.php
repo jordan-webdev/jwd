7ac173c9cfc22800576bb3ade7ee4554176385e4
jordan-webdev <jordan.trombone@gmail.com>
2018-03-02 23:35:25 -0500

    Update part-copyright.php

--- template-parts/footer/part-copyright.php
+++ template-parts/footer/part-copyright.php
@@ -5,9 +5,8 @@
  */
 ?>
 
-<div class="copyright">
-  <p class="copyright-text">
-      Copyright &copy; <?php echo Date('Y'); ?>. All Rights Reserved <?php bloginfo('title'); ?>
-      Designed and developed by <a href="http://www.dolcemedia.ca/" rel="noopener noreferrer" target="_blank">Dolce Media Group</a>
-  </p>
-</div>
+<p class="copyright-text">
+    Copyright &copy; <?php echo Date('Y'); ?>. All Rights Reserved <?php bloginfo('title'); ?>
+    Designed and developed by <a href="http://www.dolcemedia.ca/" rel="noopener noreferrer" target="_blank">Dolce Media Group</a>
+</p>
+
