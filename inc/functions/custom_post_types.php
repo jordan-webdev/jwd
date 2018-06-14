81c54e26a678b7e8640f1f73dfd96ae7ea0cdd4a
jordan-webdev <jordan.trombone@gmail.com>
2018-06-14 17:03:07 -0400

    Update custom_post_types.php

--- inc/functions/custom_post_types.php
+++ inc/functions/custom_post_types.php
@@ -1,6 +1,6 @@
 <?php
 
-// Career Custom Post Type
+ // Career Custom Post Type
  function register_career() {
 
  $labels = array(
@@ -41,7 +41,10 @@
 
  add_action( 'init', 'register_career', 0 );
 
-// FAQ Custom Post Type
+
+
+
+ // FAQ Custom Post Type
  function register_faq() {
 
  $labels = array(
@@ -84,7 +87,10 @@
 
  add_action( 'init', 'register_faq', 0 );
 
- // Glossary Custom Post Type
+
+
+
+  // Glossary Custom Post Type
   function register_glossary() {
 
   $labels = array(
@@ -127,47 +133,49 @@
 
   add_action( 'init', 'register_glossary', 0 );
 
- // Blog Custom Post Type
-function register_blog_posts()
-{
-    $labels = array(
-    'name' => __('Blog Posts'),
-    'singular_name' => __('Blog Post'),
-    'all_items' => __('All Blog Posts'),
-    'add_new' => __('Add new Blog Post'),
-    'add_new_item' => __('Add new Blog Post'),
-    'edit_item' => __('Edit Blog Post'),
-    'new_item' => __('New Blog Post'),
-    'view_item' => __('View Blog Post'),
-    'not_found' => __('No Blog Posts found'),
-    'not_found_in_trash' => __('No Blog Posts found in Trash'),
-);
 
-    $supports = array(
-    'title',
-    'editor',
-    'author',
-    'thumbnail',
-    'comments',
-    'custom-fields',
-    'revisions',
-    'page-attributes',
-    'post-formats',
-);
 
-    $args = array(
-    'labels' => $labels,
-    'public' => true,
-    'publicly_queriable' => true,  // you should be able to query it
+
+  // Blog Post Custom Post Type
+  function register_blog_post() {
+
+  $labels = array(
+    'name' => __( 'Blog Posts'),
+    'singular_name' => __( 'Blog Post'),
+    'all_items' => __( 'All Blog Posts'),
+    'add_new' => __( 'Add New Blog Post'),
+    'add_new_item' => __( 'Add New Blog Post'),
+    'edit_item' => __( 'Edit Blog Post'),
+    'new_item' => __( 'New Blog Post'),
+    'view_item' => __( 'View Blog Post'),
+    'not_found' => __( 'No Blog Posts found'),
+    'not_found_in_trash' => __( 'No Blog Posts found in Trash'),
+  );
+
+  $args = array(
+    'label'               => __( 'Blog Posts' ),
+    'description'         => __( ''),
+    'labels'              => $labels,
+    'supports'            => array( 'title', 'editor', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+    'hierarchical'        => false,
+    'public'              => true,
+    'show_ui'             => true,
+    'show_in_menu'        => true,
+    'show_in_nav_menus'   => true,
+    'show_in_admin_bar'   => true,
+    'can_export'          => true,
+    'has_archive'         => true,
+    'exclude_from_search' => false,
+    'publicly_queryable'  => true,
     'capability_type'     => 'page',
-    'show_ui' => true,  // you should be able to edit it in wp-admin
-    'exclude_from_search' => false,  // you should exclude it from search results
-    'show_in_nav_menus' => true,  // you shouldn't be able to add it to menus
-    'has_archive' => false,  // it shouldn't have archive page
-    'supports' => $supports,
-    'menu_icon' => 'dashicons-welcome-write-blog',
-    //'taxonomies' => array( 'category' ),
-);
-    register_post_type('blog_posts', $args);
-}
-add_action('init', 'register_blog_posts', 0);
+    'menu_icon'           => 'dashicons-welcome-write-blog',
+    // This is where we add taxonomies to our CPT
+    'taxonomies'          => array( 'blog_category' ),
+  );
+
+    register_post_type( 'blog_post', $args );
+
+  }
+
+  add_action( 'init', 'register_blog_post', 0 );
+
