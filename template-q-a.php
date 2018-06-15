f58a9bf0c6f20bffca53ee2080e56a100a46ac0d
jordan-webdev <jordan.trombone@gmail.com>
2018-06-15 15:50:31 -0400

    Update template-q-a.php

--- template-q-a.php
+++ template-q-a.php
@@ -13,8 +13,8 @@ get_header();
 	<div id="primary" class="content-area padding-site">
 		<main id="main" class="site-main container-site">
       <section class="page-wrapper">
-
     			<div class="grid-x grid-margin-x">
+
             <!-- Categories -->
     				<div class="cell medium-5 large-4">
 							<?php
@@ -28,19 +28,28 @@ get_header();
             <!-- Results -->
     				<div id="js-ajax-scroll-to" class="cell medium-7 large-8 js-ajax-content-container">
 							<div class="js-ajax-results">
+
 	              <?php
-								while ( have_posts() ) : the_post();
-									get_template_part('template-parts/q-a/part', 'accordions');
-	              endwhile; ?>
+									$type = esc_html(get_field("post_type"));
+									$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+									$args = array(
+										"post_type" => $type,
+										"posts_per_page" => 2,
+										"paged" => $paged,
+									);
+									$query = new WP_Query($args);
+									if ($query->have_posts()) :
+										while ( $query->have_posts() ) : $query->the_post();
+											get_template_part('template-parts/q-a/part', 'accordions');
+		              	endwhile;
+									endif; ?>
 							</div>
-
-							<!-- Pagination -->
-		          <div class="pagination">
-		            <?php //pagination(); ?>
-		          </div>
     				</div>
-    			</div>
 
+						<!-- Pagination -->
+				  	<?php pagination($query); wp_reset_postdata(); ?>
+
+    			</div>
       </section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
