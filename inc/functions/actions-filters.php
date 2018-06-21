e8b0dbd606100fe0bb5d5c0122ad478dbb348f90
jordan-webdev <jordan.trombone@gmail.com>
2018-06-21 16:47:55 -0400

    Update actions-filters.php

--- inc/functions/actions-filters.php
+++ inc/functions/actions-filters.php
@@ -32,15 +32,29 @@ function redirect_to_account(){
    *
    * img_p_class_content_filter
    * Remove <p> tags wrapping <img> in the_content()
+   * http://micahjon.com/2016/removing-wrapping-p-paragraph-tags-around-images-wordpress/
    *
    ******************************************************************
 */
-//add_filter( 'the_content', 'img_p_class_content_filter' ,20);
-function img_p_class_content_filter($content) {
-    // assuming you have created a page/post entitled 'debug'
-    $content = preg_replace("/(<p[^>]*)(\>.*)(\<img.*)(<\/p>)/im", "\$1 class='clear'\$2\$3\$4", $content);
+//add_filter( 'the_content', 'gc_remove_p_tags_around_images' ,20);
+add_filter ('acf_the_content', 'gc_remove_p_tags_around_images', 20);
+function gc_remove_p_tags_around_images($content)
+{
+	$contentWithFixedPTags =  preg_replace_callback('/<p>((?:.(?!p>))*?)(<a[^>]*>)?\s*(<img[^>]+>)(<\/a>)?(.*?)<\/p>/is', function($matches)
+	{
 
-    return $content;
+		// image and (optional) link: <a ...><img ...></a>
+		$image = $matches[2] . $matches[3] . $matches[4];
+		// content before and after image. wrap in <p> unless it's empty
+		$content = trim( $matches[1] . $matches[5] );
+		if ($content) {
+			$content = '<p>'. $content .'</p>';
+		}
+		return $image . $content;
+	}, $content);
+
+	// On large strings, this regular expression fails to execute, returning NULL
+	return is_null($contentWithFixedPTags) ? $content : $contentWithFixedPTags;
 }
 
 
