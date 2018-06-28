4e85e7f487e139d234384ee8a6993d77b853252b
jordan-webdev <jordan.trombone@gmail.com>
2018-06-28 14:50:13 -0400

    Update custom-input.js

--- js/custom-input.js
+++ js/custom-input.js
@@ -2,10 +2,9 @@
 
   // Custom radio buttons
   $('.js-radio').on("click", function() {
-    alert("click");
     var radio_item = $("#" + $(this).data("for"));
     var radio_group = $(radio_item).data("group");
-    $('.js-radio[data-group="' + radio_group + '"]').removeClass("active");
+    $('[data-group="' + radio_group + '"]').removeClass("active");
     $(radio_item).addClass("active");
 
   });
