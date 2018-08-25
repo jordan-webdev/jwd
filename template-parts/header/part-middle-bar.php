e033f67d8132cfaad4777209901beaa2a9c97cac
jordan-webdev <jordan.trombone@gmail.com>
2018-08-24 21:48:46 -0400

    Update part-middle-bar.php

--- template-parts/header/part-middle-bar.php
+++ template-parts/header/part-middle-bar.php
@@ -2,20 +2,22 @@
 /*
  * Middle bar for desktop nav
  */
+
+$is_front_page = is_front_page();
 ?>
 
 <div class="grid-x align-center">
 
   <!-- Logo -->
   <div class="logo">
-    <?php if (is_front_page()): ?>
+    <?php if ($is_front_page): ?>
       <h1>
     <?php endif; ?>
       <a href="<?php echo get_home_url(); ?>" rel="home" class="block">
         <img src="<?php echo esc_url(get_field('logo', 'options')); ?>" alt="<?php bloginfo( "title" ); ?>">
         <span class="screen-reader-text"><?php bloginfo( "name" ); ?></span>
       </a>
-    <?php if (is_front_page()): ?>
+    <?php if ($is_front_page): ?>
       </h1>
     <?php endif; ?>
   </div>
