d2f20cd54ceeddd23261766448e871a26a1ddd40
jordan-webdev <jordan.trombone@gmail.com>
2018-11-01 13:24:35 -0400

    Update template-shortcodes.php

--- inc/functions/template-shortcodes/template-shortcodes.php
+++ inc/functions/template-shortcodes/template-shortcodes.php
@@ -1,8 +1,5 @@
 <?php
 
-require get_template_directory() .'/inc/functions/template-shortcodes/contact_form.php';
 require get_template_directory() .'/inc/functions/template-shortcodes/display_careers.php';
 require get_template_directory() .'/inc/functions/template-shortcodes/google_map.php';
 require get_template_directory() .'/inc/functions/template-shortcodes/side_items.php';
-require get_template_directory() .'/inc/functions/template-shortcodes/social_media.php';
-
