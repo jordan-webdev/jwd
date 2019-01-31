289f80918ccd765f960df0240806ece3433f48fa
jordan-webdev <jordan.trombone@gmail.com>
2019-01-30 19:21:47 -0500

    Update part-header-mobile.php

--- template-parts/header/part-header-mobile.php
+++ template-parts/header/part-header-mobile.php
@@ -5,9 +5,9 @@
 
 $uploads = get_home_url() . '/wp-content/uploads';
 $mobile_logo = get_field("mobile_logo", "options");
-$phone_icon = get_field("phone_icon", "options");
-$map_icon = get_field("map_icon", "options");
-$menu_icon = get_field("menu_icon", "options");
+$phone_icon = get_field("mobile_phone_icon", "options");
+$map_icon = get_field("mobile_map_icon", "options");
+$menu_icon = get_field("mobile_menu_icon", "options");
 ?>
 
 <div class="padding-site header-mobile">
@@ -15,8 +15,8 @@ $menu_icon = get_field("menu_icon", "options");
     <nav class="menu">
 
       <!-- Logo -->
-      <a class="logo" href="<?php echo get_home_url(); ?>" rel="home">
-        <?php echo wp_get_attachment_image( $mobile_logo, "full", false, array("alt" => get_bloginfo("name")) ); ?>
+      <a class="logo" href="<?php echo get_home_url(); ?>" rel="home" style="width:<?php echo esc_attr($mobile_logo['width']); ?>px">
+        <?php echo wp_get_attachment_image( $mobile_logo['ID'], "full", false, array("alt" => get_bloginfo("name")) ); ?>
         <span class="screen-reader-text"><?php bloginfo("name"); ?>
         </span>
       </a>
@@ -24,18 +24,18 @@ $menu_icon = get_field("menu_icon", "options");
       <!-- Icons -->
       <div class="icons">
         <!-- Phone -->
-        <a class="icon-wrap phone" href="<?php echo esc_url($phone_url); ?>">
-          <?php echo wp_get_attachment_image( $phone_icon, "full", false, array("class" => "icon") ); ?>
+        <a class="icon-wrap phone" href="<?php echo esc_url($phone_url); ?>" style="width:<?php echo esc_attr($phone_icon['width']); ?>px">
+          <?php echo wp_get_attachment_image( $phone_icon['ID'], "full", false, array("class" => "icon") ); ?>
         </a>
 
         <!-- Map -->
-        <a class="icon-wrap map" href="<?php echo get_permalink(15) ?>">
-          <?php echo wp_get_attachment_image( $map_icon, "full", false, array("class" => "icon") ); ?>
+        <a class="icon-wrap map" href="<?php echo get_permalink(15) ?>" style="width:<?php echo esc_attr($map_icon['width']); ?>px">
+          <?php echo wp_get_attachment_image( $map_icon['ID'], "full", false, array("class" => "icon") ); ?>
         </a>
 
         <!-- Menu -->
-        <button id="js-mobile-menu-toggle" class="icon js-scroll-blocker" aria-controls="mobile-menu" aria-expanded="false" data-toggles="#mobile-menu">
-          <?php echo wp_get_attachment_image( $menu_icon, "full", false, array("class" => "icon", "alt" => "Toggle menu") ); ?>
+        <button id="js-mobile-menu-toggle" class="icon js-scroll-blocker" aria-controls="mobile-menu" aria-expanded="false" data-toggles="#mobile-menu" style="width:<?php echo esc_attr($menu_icon['width']); ?>px">
+          <?php echo wp_get_attachment_image( $menu_icon['ID'], "full", false, array("class" => "icon", "alt" => "Toggle menu") ); ?>
           <span class="screen-reader-text">Click to toggle the navigation menu.</span>
         </button>
       </div>
