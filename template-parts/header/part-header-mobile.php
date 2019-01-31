83bc94ba348718f53fb22e3be3f36df4c4d00030
jordan-webdev <jordan.trombone@gmail.com>
2019-01-30 19:16:52 -0500

    Update part-header-mobile.php

--- template-parts/header/part-header-mobile.php
+++ template-parts/header/part-header-mobile.php
@@ -5,25 +5,41 @@
 
 $uploads = get_home_url() . '/wp-content/uploads';
 $mobile_logo = get_field("mobile_logo", "options");
-$menu_icon = get_image_id($uploads .'/menu-min.png');
+$phone_icon = get_field("phone_icon", "options");
+$map_icon = get_field("map_icon", "options");
+$menu_icon = get_field("menu_icon", "options");
 ?>
 
-<div class="header-mobile__wrapper padding-site">
+<div class="padding-site header-mobile">
   <div class="container-site">
-    <nav class="header-mobile">
+    <nav class="menu">
 
       <!-- Logo -->
-      <a class="header-mobile__logo" href="<?php echo get_home_url(); ?>" rel="home">
+      <a class="logo" href="<?php echo get_home_url(); ?>" rel="home">
         <?php echo wp_get_attachment_image( $mobile_logo, "full", false, array("alt" => get_bloginfo("name")) ); ?>
         <span class="screen-reader-text"><?php bloginfo("name"); ?>
         </span>
       </a>
 
-      <!-- Menu -->
-      <button id="js-mobile-menu-toggle" class="header-mobile__button header-mobile__icon js-scroll-blocker" aria-controls="mobile-menu" aria-expanded="false" data-toggles="#mobile-menu">
-        <?php echo wp_get_attachment_image( $menu_icon, "full", false, array("alt" => "Toggle menu.") ); ?>
-        <span class="screen-reader-text">Click to toggle the navigation menu.</span>
-      </button>
+      <!-- Icons -->
+      <div class="icons">
+        <!-- Phone -->
+        <a class="icon-wrap phone" href="<?php echo esc_url($phone_url); ?>">
+          <?php echo wp_get_attachment_image( $phone_icon, "full", false, array("class" => "icon") ); ?>
+        </a>
+
+        <!-- Map -->
+        <a class="icon-wrap map" href="<?php echo get_permalink(15) ?>">
+          <?php echo wp_get_attachment_image( $map_icon, "full", false, array("class" => "icon") ); ?>
+        </a>
+
+        <!-- Menu -->
+        <button id="js-mobile-menu-toggle" class="icon js-scroll-blocker" aria-controls="mobile-menu" aria-expanded="false" data-toggles="#mobile-menu">
+          <?php echo wp_get_attachment_image( $menu_icon, "full", false, array("class" => "icon", "alt" => "Toggle menu") ); ?>
+          <span class="screen-reader-text">Click to toggle the navigation menu.</span>
+        </button>
+      </div>
+
 
     </nav>
   </div>
