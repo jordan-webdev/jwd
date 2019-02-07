fe57a0834edbbb0a9e18b1d2fc44206f781e2b52
jordan-webdev <jordan.trombone@gmail.com>
2019-02-07 16:10:49 -0500

    Update single-blog_post.php

--- single-blog_post.php
+++ single-blog_post.php
@@ -8,15 +8,38 @@
  * @package jwd
  */
 
-get_header(); ?>
+get_header();
+$bg = get_field("page_bg");
+$bg = $bg ? $bg : get_field("default_page_bg", "options");
+?>
+
+<?php get_template_part('template-parts/part', 'inner-hero'); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main">
 
-    <div class="padding-site">
-      <div class="container-site">
+    <div class="padding-site page-content">
+
+			<?php echo wp_get_attachment_image( $bg, "full", false, array("class" => "page-bg") ); ?>
+
+      <div class="container-site grid-x grid-margin-x">
 				<?php while ( have_posts() ) : the_post(); ?>
-	          <?php get_template_part('template-parts/blog/content', 'blog'); ?>
+
+					<!-- Categories -->
+		    	<div class="cell large-4">
+		    		<?php
+		          $sidebar = get_field("sidebar", 147);
+		          $tax = "blog_category";
+		          $title = $sidebar['title'];
+							$base_link = get_permalink(147);
+		          include(locate_template("template-parts/part-category-sidebar.php"));
+		        ?>
+		    	</div>
+
+					<div class="cell large-8">
+						<?php get_template_part('template-parts/blog/content', 'blog'); ?>
+					</div>
+
 	      <?php endwhile; ?>
       </div>
     </div>
