070a9f83648b0a6a1ec6bcf2ba91e334aa092d5b
jordan-webdev <jordan.trombone@gmail.com>
2019-03-12 12:29:52 -0400

    Update part-inner-hero.php

--- template-parts/part-inner-hero.php
+++ template-parts/part-inner-hero.php
@@ -2,6 +2,12 @@
 /*
  * Inner Hero for pages
  */
+
+$hero = get_field("inner_hero");
+$title = $hero['title'];
+$title = $title ? $title : get_the_title();
 ?>
 
-<h1 class="title"><?php echo get_the_title(); ?></h1>
+<div class="inner-hero">
+  <h1 class="title"><?php echo $title; ?></h1>
+</div>
