8556c61ef2a8e6ee2e81a4c5c5ac6ff2852fbf79
jordan-webdev <jordan.trombone@gmail.com>
2019-06-03 16:38:40 -0400

    Update part-header-mobile.php

--- template-parts/header/part-header-mobile.php
+++ template-parts/header/part-header-mobile.php
@@ -3,12 +3,12 @@
 * Mobile button, menu, logo and phone icon
 */
 
-$uploads = get_home_url() . '/wp-content/uploads';
 $mobile_logo = get_field("mobile_logo", "options");
 $phone_url = get_field("mobile_phone_url", "options");
 $phone_icon = get_field("mobile_phone_icon", "options");
 $map_icon = get_field("mobile_map_icon", "options");
 $menu_icon = get_field("mobile_menu_icon", "options");
+$contact_page_id = 95; // Update this
 ?>
 
 <div class="padding-site header-mobile">
@@ -30,7 +30,7 @@ $menu_icon = get_field("mobile_menu_icon", "options");
         </a>
 
         <!-- Map -->
-        <a class="icon-wrap map" href="<?php echo get_permalink(15) ?>" style="width:<?php echo esc_attr($map_icon['width']); ?>px">
+        <a class="icon-wrap map" href="<?php echo get_permalink($contact_page_id) ?>" style="width:<?php echo esc_attr($map_icon['width']); ?>px">
           <?php echo wp_get_attachment_image( $map_icon['ID'], "full", false, array("class" => "icon") ); ?>
         </a>
 
