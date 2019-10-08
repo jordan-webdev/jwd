b8394f9dc67dad63fbf6ec3206b1d699819b7f72
jordan-webdev <jordan.trombone@gmail.com>
2019-10-08 16:18:18 -0400

    Update carousels.js

--- js/carousels.js
+++ js/carousels.js
@@ -1,5 +1,16 @@
 (function($) {
 
+  // Main slider
+  if ($('.main-slider').length > 0) {
 
+    var main_slider = new Swiper('.main-slider', {
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+  }
 
 })(jQuery)
