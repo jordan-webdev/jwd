3969469174ac789f5a9fa58d92833f83c859fe2c
jordan-webdev <jordan.trombone@gmail.com>
2019-10-08 16:24:51 -0400

    Update carousels.js

--- js/carousels.js
+++ js/carousels.js
@@ -11,6 +11,30 @@
       },
     });
 
+    main_slider.on("slideChangeTransitionStart", function() {
+      var slider = $(".main-slider");
+      toggle_videos(slider);
+    });
+
+  }
+
+
+
+  function toggle_videos(slider) {
+
+    var active_slide = slider.find('.swiper-slide-active');
+    var all_videos = slider.find("video");
+    var active_video = active_slide.find("video");
+
+    if (all_videos.length > 0) {
+      all_videos[0].pause();
+      all_videos[0].currentTime = 0;
+    }
+
+    if (active_video.length > 0) {
+      active_video[0].play();
+    }
+
   }
 
 })(jQuery)
