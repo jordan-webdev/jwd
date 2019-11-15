da4beb4703fff4054d5754e4c87b65d8f83a6efc
jordan-webdev <jordan.trombone@gmail.com>
2019-11-15 15:14:49 -0500

    Update main-popup.js

--- js/main-popup.js
+++ js/main-popup.js
@@ -1,5 +1,14 @@
-(function($) {
+<?php
+/*
+ * Module: Main Popup
+ */
 
+$main_popup = get_field("main_popup", "options");
+?>
 
-
-})(jQuery)
+<button class="main-popup" type="button">
+  <span class="screen-reader-text">Click to close popup</span>
+  <span class="layout">
+    <?php echo wp_get_attachment_image( $main_popup['img'], "full", false, array("class" => "img") ); ?>
+  </span>
+</button>
