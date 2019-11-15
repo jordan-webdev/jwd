4f2c09076c0891696dec33f7edaed3ab029d84b8
jordan-webdev <jordan.trombone@gmail.com>
2019-11-15 15:15:58 -0500

    Update mod-main-popup.php

--- template-parts/modules/mod-main-popup.php
+++ template-parts/modules/mod-main-popup.php
@@ -6,6 +6,9 @@
 $main_popup = get_field("main_popup", "options");
 ?>
 
-<div class="main-popup">
-  <?php echo wp_get_attachment_image( $main_popup['img'], "full", false, array("class" => "img") ); ?>
-</div>
+<button class="main-popup" type="button">
+  <span class="screen-reader-text">Click to close popup</span>
+  <span class="layout">
+    <?php echo wp_get_attachment_image( $main_popup['img'], "full", false, array("class" => "img") ); ?>
+  </span>
+</button>
